d2bb7fe147dd6284d0462c62ac9c34f156a6f914
Eduardo Castro <americogomes1@outlook.com>
2024-12-16 12:18:13 -0300

    feat: update control for branch and jump instruction and add in test bench

--- MIPS_CPU/Control/control.v
+++ MIPS_CPU/Control/control.v
@@ -1,15 +1,22 @@
 module control (
 	input[31:0] instruction, 
-	output[31:0] ctrl
+	output[31:0] ctrl,
+	output [31:0] jmpAddress,
+	output reg jmpFlag
 );
 
-reg[4:0] rs;		     
-reg[4:0] rt;
-reg[4:0] rd;
+reg [4:0] rs;    
+reg [4:0] rt;
+reg [4:0] rd;
 reg [1:0] load;
-reg we_datamemory, we_registerfile, start_sel_mux4, sel_mux1, sel_mux2;
+reg we_datamemory, we_registerfile, sel_mux3, sel_mux2, sel_mux5;
+reg branchFlag;
 
-assign ctrl = {rs, rt, rd, load, we_datamemory, we_registerfile, start_sel_mux4, sel_mux1, sel_mux2, 10'b0}; 
+assign ctrl = {rs, rt, rd, load, we_datamemory, 
+			      we_registerfile, sel_mux3, sel_mux2, sel_mux5, branchFlag,
+			      9'b0}; 
+					
+assign jmpAddress = {6'b000000, instruction[25:0]};
 
 always @ (instruction) 
 	begin
@@ -19,29 +26,40 @@ always @ (instruction)
 		we_registerfile = 0;
 		we_datamemory = 0;
 		load = 2'b00; // ADD
-		start_sel_mux4 = 0;
-		sel_mux1 = 0;
+		sel_mux3 = 0;
 		sel_mux2 = 0;
+		sel_mux5 = 0;
+		jmpFlag = 0;
+		branchFlag = 0;
 		
 		// Instrucoes I
 		if(instruction[31:26] == 32'd47) 
 			begin // LW - Grupo(15)+32 = 47
 				rd = rt;
-				sel_mux1 = 1;
 				sel_mux2 = 1;
-				start_sel_mux4 = 0;
+				sel_mux5 = 1;
+				sel_mux3 = 0;
 				we_datamemory = 0;
 				we_registerfile = 1;
+				load = 2'b00; //Somar conteúdo de rs (registro A) com offset, que vem de B.
 			end 
 		
 		else if(instruction[31:26] == 32'd48) 
 			begin // SW - Grupo(15)+33 = 48
 				rd = 0;
-				sel_mux1 = 1;
 				sel_mux2 = 1;
-				start_sel_mux4 = 0;
+				sel_mux5 = 1;
+				sel_mux3 = 0;
 				we_datamemory = 1;
-				we_registerfile = 1;
+				we_registerfile = 0;
+			end
+			
+		else if (instruction[31:26] == 32'd49)
+			begin // BNE - Grupo (15) + 34 = 49
+				rd = 0;
+				branchFlag = 1;
+				load = 2'b01;
+				
 			end
 		
 		// Instruções R
@@ -49,35 +67,35 @@ always @ (instruction)
 			begin // Grupo(15)+10 = 25
 			
 				we_registerfile = 1;
-				sel_mux1 = 0;
 				sel_mux2 = 0;
+				sel_mux5 = 0;
 				
-				if(instruction[5:0] == 50) 
-					begin //MUL
-						start_sel_mux4 = 1;
+				if(instruction[5:0] == 50) //MUL
+					begin 
+						sel_mux3 = 1;
 					end
 					
-				else if (instruction[5:0] == 32) 
-					begin //ADD
+				else if (instruction[5:0] == 32) //ADD
+					begin 
 						load = 2'b00;
-						start_sel_mux4 = 0;
+						sel_mux3 = 0;
 					end
 					
-				else if(instruction[5:0] == 34) 
-					begin //SUB
+				else if(instruction[5:0] == 34) //SUB
+					begin 
 						load = 2'b01;
-						start_sel_mux4 = 0;
+						sel_mux3 = 0;
 					end
 					
-				else if(instruction[5:0] == 36) 
-					begin //AND
+				else if(instruction[5:0] == 36) //AND
+					begin 
 						load = 2'b10;
-						start_sel_mux4 = 0;
+						sel_mux3 = 0;
 					end
-				else if(instruction[5:0] == 37) 
-					begin //OR
+				else if(instruction[5:0] == 37) //OR
+					begin 
 						load = 2'b11;
-						start_sel_mux4 = 0;
+						sel_mux3 = 0;
 					end
 				else 
 					begin
@@ -87,6 +105,17 @@ always @ (instruction)
 						we_registerfile = 0;
 					end
 			end 
+			
+		else if (instruction[31:26] == 32'd02) //JMP - 02
+			begin
+				jmpFlag = 1;
+				rs = 0;
+				rt = 0;
+				rd = 0;
+				we_registerfile = 0;
+				we_datamemory = 0;
+			
+			end
 		else 
 			begin
 				rs = 0;
--- MIPS_CPU/Control/control_TB.v
+++ MIPS_CPU/Control/control_TB.v
@@ -2,40 +2,52 @@
 module control_TB();
 	reg  [31:0] instruction;
 	wire [31:0] ctrl;
+	wire [31:0] jmpAddress;
+	wire jmpFlag;
 		
 	control DUT (
 		.instruction(instruction),
-		.ctrl(ctrl)
+		.ctrl(ctrl),
+		.jmpAddress(jmpAddress),
+		.jmpFlag(jmpFlag)
 	);
 	
 	initial begin
 		//formato i       opcode| rs  |  rt |        offset
-		instruction = 32'b000010_00000_00001_0000_0011_0101_0000; // LW
+		instruction = 32'b101111_00000_00001_0000_0000_0000_0000; // LW
 		
 		#50
-		instruction = 32'b000010_00000_00010_0000_0011_0101_0001; // LW
+		instruction = 32'b101111_00000_00010_0000_0000_0000_0001; // LW
 		
 		#50 
-		instruction = 32'b000011_00001_00010_0000_0011_0101_0010; // SW
+		instruction = 32'b110000_00001_00001_0000_0000_0000_0001; // SW
 		
 		#50 
-		instruction = 32'b000011_00010_00010_0000_0011_0101_0011; // SW
+		instruction = 32'b110000_00100_00010_0000_0000_0000_0000; // SW
 		
 		#50
-		//formato R		 opcode|   rs  |  rt | rd  |     op
-		instruction = 32'b000001_00001_00010_00011_01010_110010; //MUL
+		instruction = 32'b110001_00001_00010_0000_0000_0000_1000; //BNE
 		
 		#50
-		instruction = 32'b000001_00001_00010_00110_01010_100000; //ADD
+		//formato R		 opcode  | rs  |  rt | rd  |     op
+		instruction = 32'b011001_00001_00010_00011_01010_110010; //MUL
 		
 		#50
-		instruction = 32'b000001_00001_00010_00111_01010_100010; //SUB
+		instruction = 32'b011001_00001_00010_00110_01010_100000; //ADD
 		
 		#50
-		instruction = 32'b000001_00001_00010_01000_01010_100100; //AND
+		instruction = 32'b011001_00001_00010_00111_01010_100010; //SUB
 		
 		#50
-		instruction = 32'b000001_00001_00010_01001_01010_100101; //OR
+		instruction = 32'b011001_00001_00010_01000_01010_100100; //AND
+		
+		#50
+		instruction = 32'b011001_00001_00010_01001_01010_100101; //OR
+		
+		
+		//formato J      opcode |          address
+		#50
+		instruction = 32'b000010_000000_0000_0011_0011_1010_1111;
 		
 		#50 $stop;
 	end
